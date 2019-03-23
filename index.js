36a5e0cc8ac3b1c07559af0de3cf05813234c2ab
Mohit Singh <mohitsingh1997@gmail.com>
2019-03-23 20:33:42 +0530

    Adding the backend files

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const clientApp = express();
 
 var networkInterfaces = os.networkInterfaces();
 
+serverApp.use((req, res, next) => {
+	console.log('cors middleware');
+	if((/^\/api\/3rd\/.+$/).test(req.path)) {
+		const corsOrigin = req.headers.origin;
+		const corsMethod = req.headers['access-control-request-method'];
+		const corsHeaders = req.headers['access-control-request-headers'];
+		const hasACorsFlag = corsOrigin || corMethod || corsHeaders;
+		if(hasACorsFlag) {
+			res.header('Access-Control-Allow-Origin', corsOrigin);
+			res.header('Access-Control-Allow-Methods', corsMethod);
+			res.header('Access-Control-Allow-Headers', corsHeaders);
+			res.header('Access-Control-Max-Age', 60*60*24);
+			if(req.method === 'OPTIONS') {
+				res.send(200);
+				return;
+			}
+		}
+	}
+	next();
+});
+
 const getIpAddress = () => {
 	let keys = Object.keys(networkInterfaces);
 	for(let x=0; x<keys.length; x++) {
@@ -23,7 +44,7 @@ const getIpAddress = () => {
 		}
 	}
 	return '127.0.0.1';
-}
+};
 
 const serverHost = '//' + getIpAddress() + ':' + serverPort;
 const platformScript = '/3rd/platform.js';
@@ -35,6 +56,19 @@ clientApp.get('/', (req, res) => {
 		serverHost: serverHost,
 		platformScript: platformScript,
 	});
+});
+
+serverApp.engine('js', ejs.renderFile);
+serverApp.get(platformScript, function(req, res) {
+    res.render('server'+platformScript, {
+        serverHost: serverHost,
+        platformScript: platformScript,
+    });
+});
+
+serverApp.get('/api/3rd/foo-widget/init/:id', function(req, res) {
+    var id = req.params.id;
+    res.send('api response #'+id);
 });
 
 http.createServer(serverApp).listen(serverPort);
